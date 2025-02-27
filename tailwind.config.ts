b77c52ef61f5f51d04a41decd14c13b48330ffc4
Manas kolaskar <manas.kolaskar@somaiya.edu>
2025-02-27 14:58:19 +0000

    Footer Section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
                     DEFAULT: "1rem",
                     md: "2rem",
                     lg: "4rem",
-                },
+                }
             },
             fontFamily: {
                 sans: "var(--font-archivo)"
